22d6f3a066ee538a7e9016b6bac295dac03bb44d
story <8byte@naver.com>
2021-01-29 02:30:56 +0900

    test k8s service deploy

---
+++ cont4/src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.View;
+
+@Controller
+public class IndexController {
+
+}
